6063e7dab9386169ee6113d672cde10498ecfa78
unleashy <unleashy@users.noreply.github.com>
2022-12-13 22:49:31 -0300

    Add some basic initial instructions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 /// <reference types="vitest" />
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        instructions: resolve(__dirname, "instructions.html")
+      }
+    }
+  },
+
   test: {
     include: ["tests/**/*.test.ts"]
   }
